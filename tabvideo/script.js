c6e72f80486ac3d55ca2cdbcc3a2a61a4ed0f7cb
AngelTrix01 <86498433+AngelTrix01@users.noreply.github.com>
2021-07-27 14:34:21 -0500

    Add files via upload

---
+++ tabvideo/script.js
@@ -0,0 +1,13 @@
+(Function(d))
+let tabs = Array.prototype.slice.apply(d.querySelectorAll(".tabs__item")
+let panels = Array.prototype.slice.apply(d.querySelectorAll(".panels__item");
+d.getElementById("tabs").addEventListener("click", e =>{
+	if (e.target.classList.contains("tabs__item")) {
+		let i = tabs.indexOf(e.target);
+		tabs.map(tab => tab.classList.remove("active"));
+		tabs[i].classList.add("active");
+		panels.map(panel => panel.classList.remove("active"));
+		panels[i].classList.add("active");
+	}
+});
+})(document);
